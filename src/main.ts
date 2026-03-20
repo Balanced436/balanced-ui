5b8d8a7866d7775759c55dc04532daa30c8cb7b1
SAID <ismael.saidpro@protonmail.com>
2026-03-20 12:57:09 +0400

    [refactor] flatten src/components dir

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './styles/index.css';
 import './theme.css';
 
-export { Button } from './components/molecules/button/Button.tsx';
-export { Stack } from './components/atoms/stack/Stack.tsx';
-export { Navigation } from './components/molecules/navigation/Navigation.tsx';
-export { NavigationItem } from './components/molecules/navigation/Navigation.tsx';
-export { Typography } from './components/atoms/typography/Typography.tsx';
+export { Button } from './components/button/Button.tsx';
+export { Stack } from './components/stack/Stack.tsx';
+export { Navigation } from './components/navigation/Navigation.tsx';
+export { NavigationItem } from './components/navigation/Navigation.tsx';
+export { Typography } from './components/typography/Typography.tsx';
