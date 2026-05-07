458b82115f2ff9b7d21ec4553f6cd4a27e106d74
SAID <ismael.saidpro@protonmail.com>
2026-05-07 16:16:51 +0400

    [feat] add SideBar component

---
+++ src/components/sidebar/SideBar.tsx
@@ -0,0 +1,29 @@
+import styles from './SideBar.module.css';
+import {Button, Navigation} from '../../main.ts';
+import * as React from "react";
+import {NavigationMenu} from "@base-ui/react/navigation-menu";
+
+interface SideBarProps extends React.HTMLProps<HTMLDivElement> {
+  children?: React.ReactNode;
+  justify?: 'flex-start' | 'center' | 'flex-end';
+}
+
+export function SideBar({ children, justify = 'flex-start', className, style}: SideBarProps) {
+  return (
+      <NavigationMenu.Root className={`${styles.Root} ${className}`.trim()} style={style}>
+        <NavigationMenu.List className={styles.List} data-justify={justify}>
+          {children}
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+  );
+}
+
+export function NavigationItem({ href, children }: { href: string; children: React.ReactNode }) {
+  return (
+      <NavigationMenu.Item>
+        <Button variant={'invisible'} href={href}>
+          {children}
+        </Button>
+      </NavigationMenu.Item>
+  );
+}
