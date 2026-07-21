6de451d86698cf211cd55e3969a46748f5a89326
SAID <ismael.saidpro@protonmail.com>
2026-07-21 16:20:41 +0400

    [chore] remove styled components package

--- src/components/stack/Stack.tsx
+++ src/components/stack/Stack.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import styled from 'styled-components';
-
 type Direction = 'row' | 'column' | 'row-reverse' | 'column-reverse';
 type JustifyContent = 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around';
 
@@ -11,16 +9,6 @@ interface StackProps extends React.HTMLAttributes<HTMLDivElement> {
   justifyContent: JustifyContent;
 }
 
-const StyledDiv = styled.div<StackProps>`
-  display: flex;
-  justify-content: ${({ justifyContent }) => justifyContent};
-  flex-direction: ${({ direction }) => direction};
-  gap: ${({ gap }) => `${gap}px`};
-`;
 export const Stack = ({ children, direction = 'row', gap = 0, justifyContent = 'flex-start' }: StackProps): React.JSX.Element => {
-  return (
-    <StyledDiv direction={direction} gap={gap} justifyContent={justifyContent}>
-      {children}
-    </StyledDiv>
-  );
+  return <div>{children}</div>;
 };
